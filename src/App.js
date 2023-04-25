55712b9b286d4d3055d3d64c5150a0cf96d996b7
Dibyendu Dibbo <dibyendu.cse.ru@gmail.com>
2023-04-25 16:24:15 +0600

    Product table added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,60 @@
 import { useState } from "react";
 
-function FilterableProductTable() {
+
+
+function ProductTable() {
+  const rows = [];
+  let lastCategory = null;
   
+  products.forEach((product) => {
+    if (products.category !== lastCategory) {
+      rows.push(
+        <ProductCategoryRow />
+
+      )
+    }
+    rows.push(
+      <ProductRow 
+        product={product}
+        key={product.name} />
+    )
+    lastCategory = product.category;
+  })
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </table>
+  )
+}
+
+
+function Searchbar() {
+  return (
+    <form>
+      <input type="text" placeholder="Search..." />
+      <label>
+        <input type="checkbox" />
+        {''} Only show products in stock
+      </label>
+    </form>
+  )
+}
+
+
+
+function FilterableProductTable() {
+  return (
+    <div>
+      <Searchbar />
+      <ProductTable />
+    </div>
+  )
 }
 
 
